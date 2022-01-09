ef9e89d1e2f6ebd987c4e505bee7134a33d3f340
Jonathan <91910994+Peccosbill@users.noreply.github.com>
2022-01-09 15:24:38 -0300

    Add files via upload
    Mejoras visuales Iconos

--- src/components/HandleSwitch.jsx
+++ src/components/HandleSwitch.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import "animate.css";
 import styles from "./HandleSwitch.module.css";
 import sun from "../img/sun.png";
 import moon from "../img/moon.png";
 
 function HandleSwitch({ checked, onChange, id }) {
-  console.log(checked);
   if (checked) {
     document.body.classList.remove("is-light-mode");
     document.body.classList.add("is-dark-mode");
-    console.log("darkmode");
   } else {
     document.body.classList.remove("is-dark-mode");
     document.body.classList.add("is-light-mode");
-    console.log("lightmode");
   }
   return (
     <div className={styles.darkMode}>
@@ -28,8 +26,26 @@ function HandleSwitch({ checked, onChange, id }) {
         Switch
       </label>
       <div className={styles.sunMoon}>
-        {!checked && <img src={sun} height={30} className={styles.sun} alt="sun" />}
-        {checked && <img src={moon} height={30} className={styles.moon} alt="moon" />}
+        {!checked && (
+          <img
+            src={sun}
+            height={30}
+            className={`${
+              styles.sun
+            } ${"animate__animated animate__fadeInUp"}`}
+            alt="sun"
+          />
+        )}
+        {checked && (
+          <img
+            src={moon}
+            height={30}
+            className={`${
+              styles.moon
+            } ${"animate__animated animate__fadeInDown"}`}
+            alt="moon"
+          />
+        )}
       </div>
     </div>
   );
