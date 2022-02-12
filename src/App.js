0b4bf56d5d118c8141c84d13e07c050d2a7619e7
Jonathan <91910994+Peccosbill@users.noreply.github.com>
2022-02-12 16:29:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
