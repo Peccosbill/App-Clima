d38e0bd5f3b15677f03fe5e177c2a9899b258bda
Peccosbill <jonathanmontini95@gmail.com>
2022-02-12 16:18:54 -0300

    remove redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import styles from "./App.module.css";
